7ddf55351ae19e0d90a873b6322e705774947aaa
Sean Holden <seanholden1@gmail.com>
2019-02-23 15:54:24 +0000

    Added ctags config for JS projects

---
+++ README.md
@@ -0,0 +1,7 @@
+# Install
+
+```bash
+sh install.sh
+```
+
+ctags taken from https://github.com/romainl/ctags-patterns-for-javascript
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/Dotfiles/vimrc $HOME/.config/nvim/init.vim
+ln -s $HOME/Dotfiles/ctags$HOME/.ctags
