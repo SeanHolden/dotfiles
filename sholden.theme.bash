a3473899049050b868d27b65751823994e50970d
Sean Holden <seanholden1@gmail.com>
2019-02-24 18:29:59 +0000

    Added sholden bash_it theme and kitty conf

---
+++ sholden.theme.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" ${green}|"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+__bobby_clock() {
+  if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
+    printf "$(clock_char)"
+  fi
+  printf "$(clock_prompt) "
+}
+
+__program_is_installed() {
+  local return_=1
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
+
+__node_version_prompt() {
+  if [ $(__program_is_installed node) == 1 ]; then
+    echo 👾$(node -v)
+  fi
+}
+
+__name() {
+  printf "🎮sean"
+}
+
+function prompt_command() {
+  PS1="\n⚡$(battery_char) $(__bobby_clock)${yellow}$(__node_version_prompt) ${purple}$(__name) ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}🦄→${reset_color} "
+}
+
+THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
+THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
+
+safe_append_prompt_command prompt_command
