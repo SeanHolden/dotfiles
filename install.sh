463001e13e72dc5a478d64f6211134656c7517cd
Sean Holden <sean@wakelet.com>
2021-06-30 08:57:05 +0100

    added install line for gitconfig

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 mkdir -p $HOME/.config/nvim
 rm -rf $HOME/.config/nvim/init.vim
 ln -s $HOME/Dotfiles/vimrc $HOME/.config/nvim/init.vim
+
+# gitconfig
+ln -s $HOME/Dotfiles/gitconfig $HOME/.gitconfig
