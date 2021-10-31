5adc2e3326c97510b6ff103d4e0543d0287a4b25
Sean Holden <PRIMARY GITHUB EMAIL>
2021-10-31 21:16:09 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
+# Move files from here to Dotfiles in home
+mkdir -p "$HOME"/Dotfiles
+cp vimrc "$HOME"/Dotfiles/vimrc
+cp gitconfig "$HOME"/Dotfiles/gitconfig
+
 # nvim config
-mkdir -p $HOME/.config/nvim
-rm -rf $HOME/.config/nvim/init.vim
-ln -s $HOME/Dotfiles/vimrc $HOME/.config/nvim/init.vim
+mkdir -p "$HOME"/.config/nvim
+rm -rf "$HOME"/.config/nvim/init.vim
+ln -s "$HOME"/Dotfiles/vimrc "$HOME"/.config/nvim/init.vim
 
 # gitconfig
-ln -s $HOME/Dotfiles/gitconfig $HOME/.gitconfig
+ln -s "$HOME"/Dotfiles/gitconfig "$HOME"/.gitconfig
